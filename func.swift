5369d0a7e1e6bf55fe19336a95c6bdc0eb631f8a
Thaynara Andrade <81530813+Thaynara-Andrade@users.noreply.github.com>
2022-03-29 17:47:36 -0300

    Create func.swift

---
+++ func.swift
@@ -0,0 +1,18 @@
+/
+//  File.swift
+//  academy
+//
+//  Created by Thaynara da Silva Andrade on 23/03/22.
+//
+
+import Foundation
+
+func getInt(_ msg: String) -> Int{
+    print(msg)
+    let input: String? = readLine()
+    if let string = input, let int = Int(string) {
+        return int
+    } else {
+        return -1
+    }
+}
